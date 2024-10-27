79cbe3f66e52468e92e444dd504b1ea8718f4342
Marcos Yus torio <vex.the.shadow.crow@gmail.com>
2024-10-27 11:35:52 +0100

    Add files via upload

---
+++ assets/scripts/abautme-typed.js
@@ -0,0 +1,11 @@
+var typed = new Typed("#typed-abautme", {
+    strings: ["El Tragaleche", "El Comeconchas", "El Comenardos", "La fosa de las marianas"],
+    typeSpeed: 100,  // Velocidad para escribir cada letra
+    backSpeed: 50,   // Velocidad para borrar cada letra
+    backDelay: 1000, // Tiempo de espera antes de empezar a borrar
+    startDelay: 500, // Tiempo de espera antes de empezar a escribir
+    loop: true,      // Repite el efecto de escritura y borrado en bucle
+    showCursor: true, // Muestra el cursor intermitente
+    cursorChar: "|",  // Caracter del cursor
+    contentType: 'html' // 'html' o 'null' para 'text'
+});
